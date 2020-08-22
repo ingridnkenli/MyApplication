59f850bba2b8dcd4051df07d9c423f7e8c8dfc6f
ingridnkenli <i.nkenlifack@gmail.com>
2020-08-22 09:29:14 -0400

    Testing log statements and logcat ane to display debug and error messages

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        try {
+            Log.d("MainActivity", "Happy Birthday to Me!");
+        } catch (Exception e) {
+            Log.e("MainActivity", "It's not my birthday" );
+        }
+    }
+}
